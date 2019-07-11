6a8be67b0684640ac2c212ef0a4977628fa30082
Honminden <honminden@outlook.com>
2019-07-11 17:11:31 +0800

    190711

---
+++ exercises/mask.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("%d", 0x87654302 | ~0xFF);
+  return 0;
+}
---
+++ exercises/truncate.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() 
+{
+    int x = 53191;
+    printf("%d\n", (short) x);
+
+    return 0;
+}
